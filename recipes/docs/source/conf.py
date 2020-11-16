bb151c7bdbb8dc4bd7bab32593f18fb82b55ce53
JPolton <jelt@noc.ac.uk>
2020-11-16 12:16:03 +0000

    edit conf.py for pdf

--- recipes/docs/source/conf.py
+++ recipes/docs/source/conf.py
@@ -29,7 +29,8 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['rst2pdf.pdfbuilder']
+pdf_documents = [('index', u'NEMO-RELOC', u'Relocatable Regional NEMO', u'Authors: Polton et al.'),]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
