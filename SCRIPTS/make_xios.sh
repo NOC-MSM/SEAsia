d2d97f827d55c5a83c4ca25de8a19660499e7bac
jpolton <jpolton@gmail.com>
2021-04-29 17:13:02 +0100

    Add location statement of exec

--- SCRIPTS/make_xios.sh
+++ SCRIPTS/make_xios.sh
@@ -40,5 +40,7 @@ sed -i "s/FC_MODSEARCH => ''/FC_MODSEARCH => '-J'/g" tools/FCM/lib/Fcm/Config.pm
 #recompile xios
 ./make_xios --prod --arch X86_ARCHER2-Cray --netcdf_lib netcdf4_par --job 16 --full
 
+echo "Executable is $XIOS_DIR/bin/xios_server.exe"
+
 #######################################
 cd $WDIR
