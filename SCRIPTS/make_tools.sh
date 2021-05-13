7f2d9a0bd2bd0276d9964751d1a2082836a32f58
jpolton <jpolton@gmail.com>
2021-05-13 15:38:03 +0100

    Update make_tools.sh
    update to XIOS2.5

--- SCRIPTS/make_tools.sh
+++ SCRIPTS/make_tools.sh
@@ -6,35 +6,21 @@
 #make_tools.sh
 #***********************
 #
-# At the time of writing the tools used here required an older version of XIOS to
-#work (XIOS1). This is compiled in make_xios.sh. Refer to the README in the
-# $TDIR/TOOLS/DOMAINcfg for more details.
-#
-#Also make the SOSIE tool for flood filling land in parent data when interpolating
-# onto child grid
-#'
-#::
 
   # Checkout the NEMO code from the SVN Paris repository. The tools have not
-  # been updated for a while. Consequently there is a rather clumsy nested
-  # TOOLS/TOOLS directory because a parent directory (to the child TOOLS) was
-  # required for the compatible fcm tools.
-  svn co http://forge.ipsl.jussieu.fr/nemo/svn/trunk/NEMOGCM@8395 $WDIR/BUILD_EXE/TOOLS_r8395
+  # been updated for a while. But will be soon... 
+  
+  svn co http://forge.ipsl.jussieu.fr/nemo/svn/utils/tools_r4.0-HEAD/ $WDIR/BUILD_EXE/TOOLS_HEAD
 
-
-
-  # Copy the compiler option files from the NEMO-RELOC repository clone
-  #  into the architecture directory
-  cp $WDIR/HPC_ARCH_FILES/NEMO/arch-X86_ARCHER2-Cray.fcm $WDIR/BUILD_EXE/TOOLS_r8395/ARCH/arch-X86_ARCHER2-Cray.fcm
-
-  # Edit ARCH file
-  # Dirty fix to hard wire path otherwise user will have to set XIOS_DIR in every new shell session
-  sed "s?XXX_XIOS_DIR_XXX?$XIOS1_DIR?" $WDIR/BUILD_EXE/TOOLS_r8395/ARCH/arch-X86_ARCHER2-Cray.fcm > tmp_arch
-  mv tmp_arch $WDIR/BUILD_EXE/TOOLS_r8395/ARCH/arch-X86_ARCHER2-Cray_XIOS1.fcm
-
+  cd $WDIR/BUILD_EXE/TOOLS_HEAD
+  
+  # Assuming that make_nemo.sh has already been done then all the external can be reused.
+  cp $NEMO/arch/ .
+  ln -s $NEMO/mk .
+  ln -s $NEMO/ext .
 
   # Apply patches for the weight file code
-  cd $WDIR/BUILD_EXE/TOOLS_r8395/TOOLS/WEIGHTS/src
+  cd $WDIR/BUILD_EXE/TOOLS_HEAD/WEIGHTS/src
   patch -b < $WDIR/BUILD_EXE/patch_files/scripinterp_mod.patch
   patch -b < $WDIR/BUILD_EXE/patch_files/scripinterp.patch
   patch -b < $WDIR/BUILD_EXE/patch_files/scrip.patch
@@ -45,12 +31,11 @@
   module -s restore /work/n01/shared/acc/n01_modules/ucx_env
 
   # compile tools
-  cd $WDIR/BUILD_EXE/TOOLS_r8395/TOOLS
-  ./maketools -m X86_ARCHER2-Cray_XIOS1 -n NESTING
-  ./maketools -m X86_ARCHER2-Cray_XIOS1 -n REBUILD_NEMO
-  ./maketools -m X86_ARCHER2-Cray_XIOS1 -n WEIGHTS
-  ./maketools -m X86_ARCHER2-Cray_XIOS1 -n DOMAINcfg
-
+  cd $WDIR/BUILD_EXE/TOOLS_HEAD
+  ./maketools -m X86_ARCHER2-Cray -n NESTING
+  ./maketools -m X86_ARCHER2-Cray -n REBUILD_NEMO
+  ./maketools -m X86_ARCHER2-Cray -n WEIGHTS
+  ./maketools -m X86_ARCHER2-Cray -n DOMAINcfg
 
 
   # Make SOSIE tool.
