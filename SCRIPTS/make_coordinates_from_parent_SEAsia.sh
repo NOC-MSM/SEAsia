c8b2cd6927615c958345fff7689d61c3e196ad85
not populated <jelt@noc.ac.uk>
2021-06-23 13:44:48 +0100

    update make cooards for ARCHER2:SEAsia

--- SCRIPTS/make_coordinates_from_parent_SEAsia.sh
+++ SCRIPTS/make_coordinates_from_parent_SEAsia.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-:'
-
-*******************************
-make_coordinates_from_parent.sh
-*******************************
-
-Make a netCDF coordinates file holds that holds the horizontal grid coordinate
-position and spacings information. This is used to construct the 3D version:
-the domain_cfg.nc file.
-
-This script generates a coordinates.nc file using a parent NEMO coordinates file.
-
-'
+#:'
+#
+#*******************************
+#make_coordinates_from_parent.sh
+#*******************************
+#
+# Make a netCDF coordinates file holds that holds the horizontal grid coordinate
+# position and spacings information. This is used to construct the 3D version:
+# the domain_cfg.nc file.
+#
+# This script generates a coordinates.nc file using a parent NEMO coordinates file.
+#
+#'
 #::
 
   cd $TDIR/NESTING
@@ -25,21 +25,26 @@ This script generates a coordinates.nc file using a parent NEMO coordinates file
   wget http://gws-access.jasmin.ac.uk/public/nemo/runs/ORCA0083-N06/domain/coordinates.nc -O $TDIR/NESTING/coordinates_ORCA_R12.nc
 
   # Get the namelist file from the cloned NEMO-RELOC repository
-  cp $DOMAIN/namelist.input $TDIR/NESTING/
+  cp $DOMAIN/namelist.input_SEAsia $TDIR/NESTING/namelist.input
 
-:'
-
-The namelist.input file controls the create process. Edit the bounding
-coordinates (imax, ..., jmax) and scale factor (rho, rhot) to suit the child
-coordinates. rho=rhot=7 with increase the resolution by a factor of 7.
-'
+#:'
+#
+# The namelist.input file controls the create process. Edit the bounding
+# coordinates (imax, ..., jmax) and scale factor (rho, rhot) to suit the child
+# coordinates. rho=rhot=7 with increase the resolution by a factor of 7.
+#'
 
 #::
 
-  # Generate the new coordinates file with the namelist.input settings
-  ./agrif_create_coordinates.exe
+  ## Generate the new coordinates file with the namelist.input settings using AGRIF tool
+  # Edit job script
+  sed "s?XXX_TDIR_XXX?$TDIR?" $DOMAIN/job_create_coords_template.slurm > $TDIR/NESTING/job_create_coords.slurm
+
+  # Submit the coordinates creation as a job (uses namelist.input for settings)
+  cd $TDIR/NESTING
+  sbatch job_create_coords.slurm
 
   # copy it to your DOMAIN folder where it will be used to create the domin_cfg.nc file
   cp 1_coordinates_ORCA_R12.nc $DOMAIN/coordinates.nc
 
-  cd $WORK
+  cd $WDIR/SCRIPTS
