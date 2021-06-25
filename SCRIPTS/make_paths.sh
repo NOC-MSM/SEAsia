bdf6093b237c1573da45c81f695f7e944d6c99ed
jpolton <jpolton@gmail.com>
2021-06-25 15:52:57 +0100

    Update make_paths.sh

--- SCRIPTS/make_paths.sh
+++ SCRIPTS/make_paths.sh
@@ -17,19 +17,28 @@
 #::
 
 # Paths required to compile (and run) NEMO/OPA and XIOS
+
+# Set the configuration name that describes the NEMO executable
 export CONFIG=TESTCOMPILE
-export CONFIG=NEMOhorizTS
-export CONFIG=NEMOconstTS
+export CONFIG=NEMOhorizTS # NEMO exec with hardwired horizontally uniform T,S
+export CONFIG=NEMOconstTS # NEMO exec with hardwired constant T,S
+
+# Set the path structure for the WORKING DIRECTORY. WDIR is the base directory where everything happens 
 export WORK=/work/n01/n01/$USER
-export WDIR=$WORK/NEMO-RELOC # Also the git clone directory
+export WDIR=$WORK/NEMO-RELOC # Optionally WDIR shares the same name as the repository.
+
+# Set the NEMO version
 export NEMO_VER=4.0.6
-export NEMO=$WDIR/BUILD_EXE/NEMO/$NEMO_VER
+
+# The following can then be automatically defined
+export NEMO=$WDIR/BUILD_EXE/NEMO/$NEMO_VER # base NEMO source and build path
+export XIOS_DIR=$WDIR/BUILD_EXE/XIOS/xios-2.5 # base XIOS source and build path
+
 #export CDIR=$NEMO/cfgs
 #export EXP=$WDIR/RUN_DIRECTORIES/EXPconstTS
-export XIOS_DIR=$WDIR/BUILD_EXE/XIOS/xios-2.5
-export XIOS1_DIR=$WDIR/BUILD_EXE/XIOS/xios-1.0
+#export XIOS1_DIR=$WDIR/BUILD_EXE/XIOS/xios-1.0
 
-export TDIR=$NEMO/tools
-export DOMAIN=$WDIR/BUILD_CFG/DOMAIN
+export TDIR=$NEMO/tools # base tools source and build directory
+export DOMAIN=$WDIR/BUILD_CFG/DOMAIN # base directory for building domain configuration file
 
 export DOWNLOADS=$WDIR/DOWNLOADS
