dec7bac4724704e8f21a2299e9cbfc1cc6466a7d
jpolton <jpolton@gmail.com>
2021-06-25 16:24:44 +0100

    Update make_tools.sh

--- SCRIPTS/make_tools.sh
+++ SCRIPTS/make_tools.sh
@@ -36,8 +36,6 @@
   ./maketools -m X86_ARCHER2-Cray -n REBUILD_NEMO
   ./maketools -m X86_ARCHER2-Cray -n WEIGHTS
   ./maketools -m X86_ARCHER2-Cray -n DOMAINcfg
-  ./maketools -m X86_ARCHER2-Cray -n REBUILD_NEMO
-
 
   # Make SOSIE tool.
   #$SCRIPTS/install_sosie.sh
