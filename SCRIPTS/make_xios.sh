e8aa13be5486fd60f092e12ba35a3a29fd99c49b
jpolton <jpolton@gmail.com>
2021-12-03 17:25:24 +0000

    update with new arch files + module load

--- SCRIPTS/make_xios.sh
+++ SCRIPTS/make_xios.sh
@@ -18,17 +18,17 @@
 
 cd $WDIR
 # Ensure the correct modules are loaded for ARHCER2
-module -s restore /work/n01/shared/acc/n01_modules/ucx_env
+module restore $WDIR/HPC_ARCH_FILES/envs/ucx_env_${HPC_TARG}_${COMPILER}.fcm
 
 #download xios
 svn checkout -r 1964 http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/branchs/xios-2.5 $XIOS_DIR
 cd $XIOS_DIR
 
 #copy the arch files to build location
-cp $WDIR/HPC_ARCH_FILES/XIOS/arch-X86_ARCHER2-Cray.* $XIOS_DIR/arch/.
+cp $WDIR/HPC_ARCH_FILES/XIOS/arch-${HPC_TARG}_${COMPILER}.* $XIOS_DIR/arch/.
 
 #compile xios
-./make_xios --prod --arch X86_ARCHER2-Cray --netcdf_lib netcdf4_par --job 16 --full
+./make_xios --prod --arch ${HPC_TARG}_${COMPILER} --netcdf_lib netcdf4_par --job 16 --full
 
 # First time compile will fail
 # got to $XIOS_DIR/tools/FCM/lib/Fcm/Config.pm and change
@@ -38,7 +38,7 @@ cp $WDIR/HPC_ARCH_FILES/XIOS/arch-X86_ARCHER2-Cray.* $XIOS_DIR/arch/.
 sed -i "s/FC_MODSEARCH => ''/FC_MODSEARCH => '-J'/g" tools/FCM/lib/Fcm/Config.pm
 
 #recompile xios
-./make_xios --prod --arch X86_ARCHER2-Cray --netcdf_lib netcdf4_par --job 16 --full
+./make_xios --prod --arch ${HPC_TARG}_${COMPILER} --netcdf_lib netcdf4_par --job 16 --full
 
 echo "Executable is $XIOS_DIR/bin/xios_server.exe"
 
