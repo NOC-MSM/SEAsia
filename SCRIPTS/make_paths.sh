4bdd5bca9958dff16a40ee0ee52fae8c8f3fe100
jpolton <jpolton@gmail.com>
2022-01-21 09:59:15 +0000

    Update make_paths.sh

--- SCRIPTS/make_paths.sh
+++ SCRIPTS/make_paths.sh
@@ -12,7 +12,7 @@
 # Assumed:
 # git clone https://github.com/NOC-MSM/NEMO-RELOC.git NEMO-RELOC
 
-
+export REPO=NEMO-RELOC
 # Set the configuration name that describes the NEMO executable
 export CONFIG=TESTCOMPILE
 export CONFIG=NEMOhorizTS # NEMO exec with hardwired horizontally uniform T,S
@@ -20,7 +20,7 @@ export CONFIG=NEMOconstTS # NEMO exec with hardwired constant T,S
 
 # Set the path structure for the WORKING DIRECTORY. WDIR is the base directory where everything happens 
 export WORK=/work/n01/n01/$USER
-export WDIR=$WORK/NEMO-RELOC # Optionally WDIR shares the same name as the repository.
+export WDIR=$WORK/$REPO # Optionally WDIR shares the same name as the repository.
 
 # Set the NEMO version
 export NEMO_VER=4.0.6
