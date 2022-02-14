e6a59dc9af5c9630ebf60688f7fbe69c594eb0bf
not populated <jelt@noc.ac.uk>
2022-02-14 17:37:46 +0000

    put $REPO into make_paths.sh

--- SCRIPTS/make_paths.sh
+++ SCRIPTS/make_paths.sh
@@ -10,17 +10,17 @@
 # These paths are also required to run NEMO, XIOS and various tools
 #
 # Assumed:
-# git clone https://github.com/NOC-MSM/NEMO-RELOC.git NEMO-RELOC
+# git clone https://github.com/NOC-MSM/NEMO-RELOC.git $REPO
 
-
+export REPO=NEMO-RELOC
 # Set the configuration name that describes the NEMO executable
 export CONFIG=TESTCOMPILE
 export CONFIG=NEMOhorizTS # NEMO exec with hardwired horizontally uniform T,S
-export CONFIG=NEMOconstTS # NEMO exec with hardwired constant T,S
+#export CONFIG=NEMOconstTS # NEMO exec with hardwired constant T,S
 
 # Set the path structure for the WORKING DIRECTORY. WDIR is the base directory where everything happens 
 export WORK=/work/n01/n01/$USER
-export WDIR=$WORK/NEMO-RELOC # Optionally WDIR shares the same name as the repository.
+export WDIR=$WORK/$REPO # Optionally WDIR shares the same name as the repository.
 
 # Set the NEMO version
 export NEMO_VER=4.0.6
@@ -37,6 +37,8 @@ export TDIR=$NEMO/tools # base tools source and build directory
 export DOMAIN=$WDIR/BUILD_CFG/DOMAIN # base directory for building domain configuration file
 
 export DOWNLOADS=$WDIR/DOWNLOADS
+
+export SBC=$WDIR/BUILD_CFG/SURFACE_FORCING
 
 # Set some HPC parameters to control compilation
 #export HPC_TARG="archer2-4c"
