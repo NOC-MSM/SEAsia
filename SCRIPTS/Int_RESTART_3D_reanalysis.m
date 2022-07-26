d763cfcb7483b394f144674398b539d94d13fbed
katavouta <54277836+katavouta@users.noreply.github.com>
2022-07-26 17:09:29 +0100

    Update Int_RESTART_3D_reanalysis.m

--- SCRIPTS/Int_RESTART_3D_reanalysis.m
+++ SCRIPTS/Int_RESTART_3D_reanalysis.m
@@ -63,8 +63,9 @@ Temp_in=ncread(file,[name_read]);
    % end
 
     %if you want do not waht to mask your flooded field with the nemo mask comment the
-    %lines below (your fields will look / be saved flooded)
-    Temp_out=mask_h.*Temp_out;
+    %line below (your fields will look / be saved flooded)
+    %Temp_out=mask_h.*Temp_out;
+    
     Temp_out(isnan(Temp_out))=0;
 
 %% set up  and write the netcdfi RESTART file
